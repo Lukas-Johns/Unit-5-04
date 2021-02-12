1ddc4359533dfcbd0c93a06c08711cccc947552d
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-12 14:45:49 +0000

    Added comments to JS

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.getElementById('report').addEventListener('click',myInput)
+/*Above me is the event listener connecting my button to myInput(). Below is the function that makes my text box vanish after poping up a 3D image*/
 function myVanish() {
   document.getElementById('insert').style.display = 'none';
 }
 function myInput() {
   alert(document.getElementById('insert').value);
 }
+/*Above me is the function that changes the value of my textbox into an alert. Below is the function that changes my background colour*/
 function myWallpaper() {
   document.body.style.backgroundColor = "lightgreen";
 }
